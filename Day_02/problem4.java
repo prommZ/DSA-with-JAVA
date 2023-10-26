7ed98eb2cd87a99249070efd4b600564d08d5446
PrommZ <sonukumarrani851111@gmail.com>
2023-10-26 20:12:31 +0530

    solve problem4 in day 02

--- Day_02/problem4.java
+++ Day_02/problem4.java
@@ -1,5 +1,5 @@
-public class problem4 {
-    public static void main(String[] args) {
+public class problem4{
+        public static void main(String[] args) {
         int a = 63, b= 36;
         boolean x = (a<b)? true: false;
         int y = (a>b)? a:b;
