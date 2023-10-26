99b81288df1495ee17e232cd3785de7fe2618577
PrommZ <sonukumarrani851111@gmail.com>
2023-10-26 20:09:14 +0530

    use intellij idea and add areaofcircle.class file

---
+++ Day_04/index.java
@@ -0,0 +1,19 @@
+/**
+ * index
+ */
+public class index {
+
+    public static void main(String[] args) {
+        for (int i = 1; i<= 5;i++){
+            for(int j = 1; j<=5-i;j++){
+                System.out.print(" ");
+            }
+          for(int j = 1 ; j<= i;j++){
+            System.out.print(j);
+            // System.out.println();
+          }
+           System.out.println();
+        }
+        
+    }
+}
